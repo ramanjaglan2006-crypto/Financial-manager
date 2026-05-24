9613d274c352c3bd19fd8cad2ca3b21e8ca07555
raman <raman@ramans-MacBook-Air.local>
2026-05-24 21:37:23 +0530

    Add AppUserDetailsService.java

---
+++ src/main/java/com/financemanager/security/AppUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.financemanager.security;
+
+import com.financemanager.entity.User;
+import com.financemanager.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public AppUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(), user.getPassword(), Collections.emptyList());
+    }
+}
